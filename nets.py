bf31b6945d483a253cf201cb7a92de873f10be2f
D Near <dan.e.near@gmail.com>
2020-05-23 20:00:26 -0500

    Create nets.py

---
+++ nets.py
@@ -0,0 +1,7 @@
+#Don't check this data into Github!!
+#code modified from https://docs.pycom.io/tutorials/all/wlan.html
+known_nets = {
+    'first': {'pwd': 'firstpwd'},
+    'second': {'pwd': 'secondpwd'},
+    'third': {'pwd': 'thirdpwd'}#, 'wlan_config':  ('10.0.0.114', '255.255.0.0', '10.0.0.1', '10.0.0.1')}, # (ip, subnet_mask, gateway, DNS_server)
+}
